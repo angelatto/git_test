82f24bd9a32bb5dfd667f198d933594be476ec30
angelatto <pudri109@gmail.com>
2020-01-14 15:27:53 +0900

    third commit

--- TestClass.java
+++ TestClass.java
@@ -3,7 +3,7 @@ public class TestClass {
 
     public static void main(String[] args){
 
-        System.out.println("Hi~~~~~");
+        System.out.println("Hi~~hello~~~");
         System.out.println("I'm chae jeong");
     }
 
