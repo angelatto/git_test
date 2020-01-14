65b7947fcf3dc181e6b3d3046352e1ba51fb6ac1
angelatto <pudri109@gmail.com>
2020-01-14 15:29:41 +0900

    bye commit

--- TestClass.java
+++ TestClass.java
@@ -3,7 +3,7 @@ public class TestClass {
 
     public static void main(String[] args){
 
-        System.out.println("Hi~~~~~");
+        System.out.println("bye");
         System.out.println("I'm chae jeong");
     }
 
